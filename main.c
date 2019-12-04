acad610c7e2793c8776f75ef4779dad05d55d539
Fellipe Calleia <fellipe@calleia.com.br>
2019-12-03 21:21:12 -0300

    Add command line arguments

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
+    if (argc == 3) {
+        FILE* pFirstFilename = fopen(argv[1], "r");
+        FILE* pSecondFilename = fopen(argv[2], "r");
+
+        // TODO: read images
+        // TODO: calculate hamming distance
+    } else {
+        fprintf(stderr, "Invalid number of arguments.");
+    }
+
     return 0;
 }
